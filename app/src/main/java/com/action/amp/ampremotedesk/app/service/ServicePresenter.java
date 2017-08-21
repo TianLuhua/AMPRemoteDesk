3e0af3b4d4af3c09d55a17ca076c93aaa332c71c
Tianluhua <466469918@qq.com>
2017-08-21 10:28:48 +0800

    基础的修改

--- app/src/main/java/com/action/amp/ampremotedesk/app/service/ServicePresenter.java
+++ app/src/main/java/com/action/amp/ampremotedesk/app/service/ServicePresenter.java
@@ -1,9 +1,21 @@
 package com.action.amp.ampremotedesk.app.service;
 
+import com.action.amp.ampremotedesk.app.main.MainContract;
+
 /**
  * Created by tianluhua on 2017/8/17 0017.
  */
 public class ServicePresenter implements ServiceContract.Presenter {
+
+
+
+    private ServiceContract.View view;
+
+
+    public ServicePresenter(ServiceContract.View view) {
+        this.view = view;
+        this.view.setPresenter(this);
+    }
 
     @Override
     public void start() {
--- app/src/main/java/com/action/amp/ampremotedesk/app/settings/SettingPresenter.java
+++ app/src/main/java/com/action/amp/ampremotedesk/app/settings/SettingPresenter.java
@@ -4,6 +4,15 @@ package com.action.amp.ampremotedesk.app.settings;
  * Created by Administrator on 2017/8/17 0017.
  */
 public class SettingPresenter implements SettingContract.Presenter {
+
+
+    private SettingContract.View view;
+
+    public SettingPresenter(SettingContract.View view) {
+        this.view = view;
+        this.view.setPresenter(this);
+    }
+
     @Override
     public void start() {
 
